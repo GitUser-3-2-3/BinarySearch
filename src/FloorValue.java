11ae9e49c6750ab00bc68dbfe21affe17abbcb2f
GitUser-3-2-3 <parthsrivastav.00@gmail.com>
2023-12-15 16:49:25 +0530

    Binary Search Algorithms

---
+++ src/FloorValue.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class FloorValue {
+    public static void main(String[] args) {
+        System.out.println();
+
+        System.out.println("Enter the target value: ");
+        Scanner sc = new Scanner(System.in);
+
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 11, 18};
+        int target = sc.nextInt();
+        System.out.println(binarySearch(arr, target));
+    }
+
+    static int binarySearch(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (arr[mid] == target) {
+                return mid;
+            } else if (target < arr[mid]) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+
+        return arr[end];
+    }
+}
